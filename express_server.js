5b5f3cb9f416843493b961031a5c599fb0d686cd
EVillett <emily.villett@gmail.com>
2022-10-03 18:39:08 -0700

    added generate random string function for the short url path

--- express_server.js
+++ express_server.js
@@ -10,14 +10,35 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+function generateRandomString(length) {
+  let result = '';
+  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+};
+
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: 'http://www.lighthouselabs.ca' };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send(generateRandomString(6)); // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/", (req, res) => {
